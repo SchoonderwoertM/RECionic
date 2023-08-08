b13f7c1f8fd2a70e2afbb0e823a9a87583639220
SchoonderwoertM <mariska.sdw@gmail.com>
2023-08-08 19:27:35 +0200

    vite reinstalled

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [
     vue(),
     legacy()
@@ -18,4 +17,4 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom'
   }
-})
+}
